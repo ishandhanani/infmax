24c165729f497cb6820e6e18406e0adf603c21fa
Cam Quilici <cjquilici@gmail.com>
2026-01-20 11:46:07 -0600

    initial commit

--- runners/launch_mi355x-amd.sh
+++ runners/launch_mi355x-amd.sh
@@ -137,12 +137,12 @@ PY
 
 else
 
-    export HF_HUB_CACHE_MOUNT="/hf-hub-cache"
+    export HF_HUB_CACHE_MOUNT="/it-share/hf-hub-cache/"
     export PORT_OFFSET=${USER: -1}
     FRAMEWORK_SUFFIX=$([[ "$FRAMEWORK" == "atom" ]] && printf '_atom' || printf '')
 
     PARTITION="compute"
-    SQUASH_FILE="/squash/$(echo "$IMAGE" | sed 's/[\/:@#]/_/g').sqsh"
+    SQUASH_FILE="/it-share/squash/$(echo "$IMAGE" | sed 's/[\/:@#]/_/g').sqsh"
 
     if [[ "$MODEL" == "amd/DeepSeek-R1-0528-MXFP4-Preview" || "$MODEL" == "deepseek-ai/DeepSeek-R1-0528" ]]; then
         if [[ "$OSL" == "8192" ]]; then
@@ -154,17 +154,15 @@ else
         export NUM_PROMPTS=$(( CONC * 10 ))
     fi
 
-    export ENROOT_RUNTIME_PATH=/tmp
-
     set -x
-    salloc --partition=$PARTITION --gres=gpu:$TP --cpus-per-task=256 --time=180 --no-shell
+    salloc --partition=$PARTITION --gres=gpu:$TP --cpus-per-task=128 --time=180 --no-shell
     JOB_ID=$(squeue -u $USER -h -o %A | head -n1)
 
     if [[ "$FRAMEWORK" == "atom" ]]; then
-        srun --jobid=$JOB_ID bash -c "sudo rm $SQUASH_FILE"
+        srun --jobid=$JOB_ID bash -c "rm $SQUASH_FILE"
     fi
-    srun --jobid=$JOB_ID bash -c "sudo enroot import -o $SQUASH_FILE docker://$IMAGE"
-    srun --jobid=$JOB_ID bash -c "sudo chmod -R a+rwX /hf-hub-cache/"
+    srun --jobid=$JOB_ID bash -c "enroot import -o $SQUASH_FILE docker://$IMAGE"
+    # srun --jobid=$JOB_ID bash -c "sudo chmod -R a+rwX /hf-hub-cache/"
     srun --jobid=$JOB_ID \
         --container-image=$SQUASH_FILE \
         --container-mounts=$GITHUB_WORKSPACE:/workspace/,$HF_HUB_CACHE_MOUNT:$HF_HUB_CACHE \
