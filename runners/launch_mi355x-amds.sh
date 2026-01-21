7438bc9ca0c597e4ae76a388bcc2eb1f11538476
Cam Quilici <cjquilici@gmail.com>
2026-01-21 10:40:39 -0600

    remove docker containers on mi355x pt 2

--- runners/launch_mi355x-amds.sh
+++ runners/launch_mi355x-amds.sh
@@ -158,7 +158,7 @@ else
     salloc --partition=$PARTITION --gres=gpu:$TP --cpus-per-task=128 --time=180 --no-shell --job-name="$RUNNER_NAME"
     JOB_ID=$(squeue --name="$RUNNER_NAME" -h -o %A | head -n1)
 
-    srun --jobid=$JOB_ID bash -c "docker stop $(docker ps -a -q)"
+    srun --jobid=$JOB_ID bash -c "docker stop \$(docker ps -a -q)"
 
     if [[ "$FRAMEWORK" == "atom" ]]; then
         srun --jobid=$JOB_ID bash -c "rm $SQUASH_FILE"
