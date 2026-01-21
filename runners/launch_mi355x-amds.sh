abd351e02a8f560d8decdaf87489c5922af8a0fd
Cam Quilici <cjquilici@gmail.com>
2026-01-21 10:22:22 -0600

    remove docker containers on mi355x

--- runners/launch_mi355x-amds.sh
+++ runners/launch_mi355x-amds.sh
@@ -158,15 +158,19 @@ else
     salloc --partition=$PARTITION --gres=gpu:$TP --cpus-per-task=128 --time=180 --no-shell --job-name="$RUNNER_NAME"
     JOB_ID=$(squeue --name="$RUNNER_NAME" -h -o %A | head -n1)
 
+    srun --jobid=$JOB_ID bash -c "docker stop $(docker ps -a -q)"
+
     if [[ "$FRAMEWORK" == "atom" ]]; then
         srun --jobid=$JOB_ID bash -c "rm $SQUASH_FILE"
     fi
+
     srun --jobid=$JOB_ID bash -c "enroot import -o $SQUASH_FILE docker://$IMAGE"
     if ! srun --jobid=$JOB_ID bash -c "unsquashfs -l $SQUASH_FILE > /dev/null"; then
         echo "unsquashfs failed, removing $SQUASH_FILE and re-importing..."
         srun --jobid=$JOB_ID bash -c "rm -f $SQUASH_FILE"
         srun --jobid=$JOB_ID bash -c "enroot import -o $SQUASH_FILE docker://$IMAGE"
     fi
+
     srun --jobid=$JOB_ID \
         --container-image=$SQUASH_FILE \
         --container-mounts=$GITHUB_WORKSPACE:/workspace/,$HF_HUB_CACHE_MOUNT:$HF_HUB_CACHE \
